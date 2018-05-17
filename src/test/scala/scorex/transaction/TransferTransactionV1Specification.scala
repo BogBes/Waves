70b12de6950014871192a6c9d9351014d67bc596
Nastya.Urlapova <aurlapova@wavesplatform.com>
2018-05-17 19:34:22 +0300

    NODE-755 Back-compatibility unit tests for transactions

--- src/test/scala/scorex/transaction/TransferTransactionV1Specification.scala
+++ src/test/scala/scorex/transaction/TransferTransactionV1Specification.scala
@@ -1,9 +1,12 @@
 package scorex.transaction
 
-import com.wavesplatform.TransactionGen
+import com.wavesplatform.{TransactionGen, state}
+import com.wavesplatform.state.ByteStr
 import org.scalatest._
 import org.scalatest.prop.PropertyChecks
+import scorex.account.{Address, AddressOrAlias, Alias, PublicKeyAccount}
 import scorex.transaction.transfer._
+import play.api.libs.json._
 
 class TransferTransactionV1Specification extends PropSpec with PropertyChecks with Matchers with TransactionGen {
 
@@ -30,4 +33,39 @@ class TransferTransactionV1Specification extends PropSpec with PropertyChecks wi
     }
   }
 
+  property("json format validation") {
+    val js = """
+      "type": 4,
+    "id": "3RkWfTXd4akphqNi9ggoyDzs7XeAg63md9dior1ESbo8",
+    "sender": "3MvbZaWThxGQ7YE3FA6jc7KLj6EXhhYS7Xy",
+    "senderPublicKey": "88evu5gxTdCaR35eYr4roSCADDhhQmJDxHxZ9VQtudWV",
+    "fee": 100000,
+    "timestamp": 1526552510868,
+    "signature": "2sqCfXfDLgqofMMkPPUWbp5kP4VcznmiGpStNE9MNNzv6ZTjFBPpfaJDfhp9hf2aoZWVrbRWNKWbMT6hwYkSp974",
+    "version": 1,
+    "recipient": "3MzfTZcrLjntFY1jLMUcSgkzhg349v2MmZX",
+    "assetId": null,
+    "feeAssetId": null,
+    "amount": 1900000,
+    "attachment": ""
+    """
+
+    val tx1 = TransferTransactionV1
+      .create(
+        None,
+        PublicKeyAccount.fromBase58String("88evu5gxTdCaR35eYr4roSCADDhhQmJDxHxZ9VQtudWV").right.get,
+        Address.fromString("3MzfTZcrLjntFY1jLMUcSgkzhg349v2MmZX").right.get,
+        1900000,
+        BigInt(1526552510868L).longValue(),
+        None,
+        100000,
+        "".getBytes,
+        ByteStr("2sqCfXfDLgqofMMkPPUWbp5kP4VcznmiGpStNE9MNNzv6ZTjFBPpfaJDfhp9hf2aoZWVrbRWNKWbMT6hwYkSp974".getBytes)
+      )
+      .right
+      .get
+
+    val j = tx1.json()
+    js shouldEqual j
+  }
 }
